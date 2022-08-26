76d2279ac8ea4b5ed89723d2ac5625be9f8fe338
Taj <sharma.tajinder01@gmail.com>
2022-08-25 22:44:00 -0700

    Add solution for constains duplicate

--- hashmaps.js
+++ hashmaps.js
@@ -15,7 +15,27 @@ var singleNumber = function(nums) {
         }
     }return(Object.keys(hashMap)[0])
 } 
+//let list = [1,3,3,4,5,5,4]
+//console.log(singleNumber(list))
 
-let list = [1,3,3,4,5,5,4]
 
-console.log(singleNumber(list))
+
+
+//Solution for lc #217: contains duplicate
+//checks if an array of numbers contains a duplicate
+var containsDuplicate = function(nums){
+    let hash = {};
+
+    for(let i =0; i <nums.length; i++){
+        if(hash[nums[i]]){
+            return true;
+        }else{
+            hash[nums[i]]=true;
+        }
+    }return  false;
+}
+
+//let nums1= [1,2,3,4,5];
+//console.log(containsDuplicate(nums1))
+
+
