e33ee04baf5197b0e484b20238034adecaab8e27
Taj <sharma.tajinder01@gmail.com>
2022-08-29 01:26:07 -0700

    Update anagram solution comments

--- dict.py
+++ dict.py
@@ -1,9 +1,10 @@
-
-#practice this one again
+#leetCode 242: Is anagram
+#another way to solve is to simply sort and return if both are equal
+#or we can use dictionaries for the counting method
 def isAnagram2(self, s, t):
-    dic1, dic2 = [0]*26, [0]*26
+    dic1, dic2 = [0]*26, [0]*26 #creates sempty dictionaries w 26 characters
     for item in s:
-        dic1[ord(item)-ord('a')] += 1
+        dic1[ord(item)-ord('a')] += 1 #ord() returns number rep of unicode
     for item in t:
-        dic2[ord(item)-ord('a')] += 1
-    return dic1 == dic2
+        dic2[ord(item)-ord('a')] += 1 
+    return dic1 == dic2  #if dicts are equal then it returns true
