e2bda192dca4c4bead590d2ddcbdfa66f41741d8
Taj <sharma.tajinder01@gmail.com>
2022-08-28 17:34:23 -0700

    Add mergetwoLists solution

--- linked.js
+++ linked.js
@@ -1,5 +1,19 @@
 //lc #21: Merge Two sorted lists
 
 var mergeTwoLists = function(list1, list2) {
-    
+    var Dummy = {var:-1, next:  null};
+    let current = Dummy;
+
+    while(list1 && list2){
+        if(list1.val < list2.val){
+            current.next = list1;
+            list1 = list1.next
+        }else{
+            current.next = list2;
+            list2 = list2.next;
+        }current = current.next;
+    }
+    current.next = list1 || list2;
+
+    return Dummy.next;
 };
