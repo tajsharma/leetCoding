89cbbdf4e4f71e5bd90d9bcc370f54294b6a12ef
Taj <sharma.tajinder01@gmail.com>
2022-11-07 11:47:11 -0800

    Create init method for solution class

---
+++ dfs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def __init__(self) -> None:
+        pass
+
+    def subsets(nums):
+        result = []
+
+        subset =[]
+
+        def dfs(i):
+            for i in nums:
+                print(nums[i])
+
+
+lebron = Solution
+num = [1,2,3]
+
+lebron.subsets(num)
