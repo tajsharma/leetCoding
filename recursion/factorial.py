b3c4732eda1315d67d91622b9d462ff7a4cfc887
Taj <sharma.tajinder01@gmail.com>
2022-12-18 17:43:45 -0800

    Add condition to stop decimal entries

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,9 +1,11 @@
 def factorial(n):
     if n <= 1:
         return 1 
+    elif n%1 != 0:
+        return "Ur number was a decimal"
     else:
         return n * factorial(n-1)
 
 
         
-print(factorial(-2))
+print(factorial(3))
