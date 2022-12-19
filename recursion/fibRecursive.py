fb5bd36a89e1a3a015871713ba63d83c57f0a84c
Taj <sharma.tajinder01@gmail.com>
2022-12-18 18:19:11 -0800

    comment the assert function out"

--- recursion/fibRecursive.py
+++ recursion/fibRecursive.py
@@ -1,4 +1,5 @@
 def fib(n):
+    #assert n==0 and int(n) ==n , 'Cant put 0 or a decimal here brotha'
     if n ==1:
         return 0
     if n == 2:
@@ -6,4 +7,4 @@ def fib(n):
     else:
         return fib(n-1)+ fib(n-2)
 
-print(fib(4))
+print(fib(8))
