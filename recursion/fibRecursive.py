5cd9b97ae39d302d31aa41e9a4ef797e5d8243dc
Taj <sharma.tajinder01@gmail.com>
2022-12-18 18:30:23 -0800

    uncomment and fix the assert function

--- recursion/fibRecursive.py
+++ recursion/fibRecursive.py
@@ -1,5 +1,5 @@
 def fib(n):
-    #assert n==0 and int(n) ==n , 'Cant put 0 or a decimal here brotha'
+    assert n>=0 and int(n)==n , '# gotta be greater than 0 and not a decimal brotha'
     if n ==1:
         return 0
     if n == 2:
