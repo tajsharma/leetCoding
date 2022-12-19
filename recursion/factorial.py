72c15001bd5946382fda155a2734ca32552b7931
Taj <sharma.tajinder01@gmail.com>
2022-12-18 17:47:03 -0800

    Change the code to have an assert method at the top

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,11 +1,10 @@
 def factorial(n):
+    assert n>0 and int(n), 'The number you entered is not a valid number'
     if n <= 1:
         return 1 
-    elif n%1 != 0:
-        return "Ur number was a decimal"
     else:
         return n * factorial(n-1)
 
 
         
-print(factorial(3))
+print(factorial(6))
