3e607d5e511c5ca6c88f9df35c2f7d1698b4fc61
Taj <sharma.tajinder01@gmail.com>
2022-12-18 17:50:52 -0800

    fix syntax in the assert function

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,5 +1,5 @@
 def factorial(n):
-    assert n>0 and int(n), 'The number you entered is not a valid number'
+    assert n>=0 and int(n) ==n, 'The number you entered is not a valid number'
     if n <= 1:
         return 1 
     else:
