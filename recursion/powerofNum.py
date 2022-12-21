fd8676d3166dd920d389bde4191826fa03d6a6ac
Taj <sharma.tajinder01@gmail.com>
2022-12-21 00:43:34 -0800

    Add assert function to catch decimals and negative exponents

--- recursion/powerofNum.py
+++ recursion/powerofNum.py
@@ -1,6 +1,7 @@
 def powerofNums(n,power):
+    assert int(n)== n and power>0, 'error with entries'
     if power == 1:
         return n 
     else:
         return n * powerofNums(n,power-1)
-print(powerofNums(5,6))
+print(powerofNums(2,6))
