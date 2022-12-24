e94ef7b14ca8cf80aa9a2c14506297f71b3a7ef5
Taj <sharma.tajinder01@gmail.com>
2022-12-23 19:02:59 -0800

    Append a value to the array

--- pythonArrays/array1.py
+++ pythonArrays/array1.py
@@ -1,6 +1,13 @@
 from array import *
 
+#1 create the array
 arr1 = array('i',[77,23,13,124])
 
-arr1.remove(77)
+
+#2 traverse the array and print each element
+for i in range(0,len(arr1)):
+    print(arr1[i])
+
+#3 appned a value to the array
+arr1.append(99)
 print(arr1)
