d71460a71402062b5dea5bcf79be1fe76fdf8ff1
Taj <sharma.tajinder01@gmail.com>
2022-12-24 20:35:37 -0800

    Leave return comment

--- lists/list1.py
+++ lists/list1.py
@@ -1,2 +1,17 @@
-list1 = [1,2,3,'pop',[1,2,23]]
-print(list1[4][2])
+list1 = [1,2,3,5,9,123,32,14,45,69,345]
+list1.sort()
+
+
+def binarySearch(list,value): # NEED TO COME BACK AND FIX THIS ONE
+    midIndex = len(list1)//2
+    if list[midIndex] == value:
+        return 'Value has been found'
+    elif list[midIndex] > value:
+        return binarySearch(list[0:midIndex-1],value)
+    else:
+        return binarySearch(list[midIndex+1:len(list)],value)
+    
+    
+
+
+binarySearch(list1,123)
