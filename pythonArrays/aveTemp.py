2dd53035edf3d66e7227c075ccfa6aa9aed35469
Taj <sharma.tajinder01@gmail.com>
2022-12-28 15:52:39 -0800

    Change value of each element in array and then add print statement to test

--- pythonArrays/aveTemp.py
+++ pythonArrays/aveTemp.py
@@ -1,6 +1,10 @@
 import numpy as np 
 
 numberOfDays = int(input("How many days do you want to submit the temperature? "))
+tempArray = [None]*numberOfDays
 
 for i in range(0,numberOfDays):
     temp = int(input(f"What is the temperature for day #{i+1}: "))
+    tempArray[i] = temp
+
+print(tempArray)
