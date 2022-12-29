fc362df0204105dea09362ce7bc8c035b3f3b2d7
Taj <sharma.tajinder01@gmail.com>
2022-12-28 16:00:02 -0800

    Print days the temperature was higher than the average

--- pythonArrays/aveTemp.py
+++ pythonArrays/aveTemp.py
@@ -2,9 +2,16 @@ import numpy as np
 
 numberOfDays = int(input("How many days do you want to submit the temperature? "))
 tempArray = [None]*numberOfDays
+count = 0
 
 for i in range(0,numberOfDays):
     temp = int(input(f"What is the temperature for day #{i+1}: "))
     tempArray[i] = temp
 
-print(tempArray)
+print(f"The average temp over {len(tempArray)} day(s) was: {sum(tempArray)/len(tempArray)} degrees")
+
+for temps in tempArray:
+    if temps > sum(tempArray)/len(tempArray):
+        count +=1
+
+print(f"There were {count} day(s) where the temp was higher than average")
