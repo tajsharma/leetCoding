10dcec98342cde8b69c5a34103de950671e00be7
Taj <sharma.tajinder01@gmail.com>
2023-01-01 15:45:31 -0800

    Note to self; finish this question after matrix

---
+++ pythonArrays/rotateMatrix.py
@@ -0,0 +1,15 @@
+# given a n x n 2d matrix , rotate the matrix(representing an image) 90 degrees clockwise
+import numpy as np 
+
+
+def rotate90(array2d):
+    pass
+
+
+
+
+og = np.array[[[1,2,3],
+                [4,5,6],
+                [7,8,9]]]
+    
+print(rotate90(og))
