a1b55e6c07a3003334f2446b8bbd3c81675549db
Taj <sharma.tajinder01@gmail.com>
2023-01-01 20:15:11 -0800

    Pirnt dictionary after putting it through different methods

--- dictionaries/dict.py
+++ dictionaries/dict.py
@@ -1,5 +1,6 @@
 myDict = {'student1':'Lebron James', 'student2':'Stephen Curry', 'student3':'Kevin Durant'}
 
+#linear search 
 def searchDict(dict, value):
     for key in dict:
         if dict[key] == value:
@@ -7,3 +8,9 @@ def searchDict(dict, value):
     return False
 
 print(searchDict(myDict,'Lebron J2ames'))
+
+
+#deletion
+print(myDict.popitem())
+
+print(myDict)
