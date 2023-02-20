23cdaa6c0fe46d4a2561f31e778720031a2ce2ad
Taj <sharma.tajinder01@gmail.com>
2023-02-20 00:41:59 -0800

    Create empty method for dfs implementation, compelete bfs code

--- graphs_trees_visualization/basicGraph.py
+++ graphs_trees_visualization/basicGraph.py
@@ -18,7 +18,18 @@ class Graph:
             return True #if added
         return False #if not added to the graph
 
-    def bfs(self):
+    def bfs(self,start_vertex):
+        visited = [start_vertex]
+        queue = [start_vertex]
+        while queue:#while queue is not empty 
+            dq_vertex = queue.pop(0)
+            print(dq_vertex)
+            for adj_vertex in self.graphDict[dq_vertex]:
+                if adj_vertex not in visited:
+                    visited.append(adj_vertex)
+                    queue.append(adj_vertex)
+
+    def dfs(self,start_vertex):
         pass
 
 customDict = {
