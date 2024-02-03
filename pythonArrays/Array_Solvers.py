b675f597dfb099e4797ec198de1a29515024996b
Taj Sharma <sharma.tajinder01@gmail.com>
2024-02-03 15:57:51 -0800

    Add main file for running scripts and class for array problem solvers

---
+++ pythonArrays/Array_Solvers.py
@@ -0,0 +1,7 @@
+class ArraySolvers:
+
+    def __init__(self,name):
+        self.name = name
+    
+    def maximum_Subarray_BF(self,nums):
+        maxSum = float('-inf')
---
+++ pythonArrays/main.py
@@ -0,0 +1,4 @@
+from Array_Solvers import ArraySolvers
+
+a1 = ArraySolvers()
+
