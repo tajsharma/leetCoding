2c2cbad1eaf522e1cbad5f70d01c51b301620dec
Taj Sharma <sharma.tajinder01@gmail.com>
2024-02-03 16:16:38 -0800

    Change the array values for testing, remove the -1 from inner loop as it was causing addition errors

--- pythonArrays/Array_Solvers.py
+++ pythonArrays/Array_Solvers.py
@@ -1,7 +1,19 @@
 class ArraySolvers:
 
-    def __init__(self,name):
-        self.name = name
+    def __init__(self):
+        pass
     
     def maximum_Subarray_BF(self,nums):
         maxSum = float('-inf')
+        currentSum = 0
+
+        for i in range (0,len(nums)-1):
+            currentSum += nums[i]
+            for j in range(i+1,len(nums)-1):
+                currentSum+=nums[j]
+                if currentSum > maxSum:
+                    maxSum = currentSum
+                    print("current max is: ", maxSum)
+            currentSum = 0
+
+        return maxSum
--- pythonArrays/main.py
+++ pythonArrays/main.py
@@ -2,3 +2,6 @@ from Array_Solvers import ArraySolvers
 
 a1 = ArraySolvers()
 
+array2 = [1]
+
+print(a1.maximum_Subarray_BF(array2))
